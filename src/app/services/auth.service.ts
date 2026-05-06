5f391a961c908f39269a4f7eac38c39ba271b83c
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2026-05-06 10:48:40 -0300

    feat: adicionando autorização

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { UserResponse, LoginRequest, UserRegisterRequest } from '../models/auth.model';
 
 @Injectable({
@@ -9,14 +10,43 @@ import { UserResponse, LoginRequest, UserRegisterRequest } from '../models/auth.
 export class AuthService {
   
   private readonly API_URL = 'http://localhost:8080/api/users';
-  private readonly API_URL_AUTH = 'http://localhost:8080/api/users';
-  constructor(private http: HttpClient) { }
+  private loggedIn = new BehaviorSubject<boolean>(this.checkToken());
+
+  constructor(private http: HttpClient, private router: Router) { }
+  get isLoggedIn$(): Observable<boolean> {
+    return this.loggedIn.asObservable();
+  }
+
+  isLoggedIn(): boolean {
+    return this.loggedIn.value;
+  }
+
+  private checkToken(): boolean {
+    return !!localStorage.getItem('user_info');
+  }
 
   login(credentials: LoginRequest): Observable<UserResponse> {
-    return this.http.post<UserResponse>(`${this.API_URL_AUTH}/auth`, credentials, { withCredentials: true });
+    return this.http.post<UserResponse>(`${this.API_URL}/auth`, credentials, { withCredentials: true })
+      .pipe(
+        tap(user => {
+          localStorage.setItem('user_info', JSON.stringify(user));
+          this.loggedIn.next(true); 
+        })
+      );
   }
 
   register(userData: UserRegisterRequest): Observable<any> {
     return this.http.post(`${this.API_URL}`, userData);
+  }
+
+  logout() {
+    localStorage.removeItem('user_info');
+    this.loggedIn.next(false); 
+    this.router.navigate(['/login']);
+  }
+
+  getUserName(): string {
+    const user = localStorage.getItem('user_info');
+    return user ? JSON.parse(user).nome : '';
   }
 }
